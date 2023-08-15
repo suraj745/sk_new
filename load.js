0eadb4888bfc47064cce91252e27870695381e22
suraj745 <surajarya745@gmail.com>
2023-08-16 00:41:20 +0530

    almost-done

--- load.js
+++ load.js
@@ -1,1 +1,10 @@
-$(".load_navbar").load("./pages/Navbar/Navbar.html");
+import $ from "jquery";
+
+$(".load_navbar").load("/pages/Navbar/Navbar.html");
+$(".load_footer").load("/pages/Footer/Footer.html");
+
+var loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+  loader.style.display = "none";
+});
